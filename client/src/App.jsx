662d0dd8ab54e062e647409e625cb8f8f1f9e2fd
rishabhsharmaa <rishabh122005@gmail.com>
2025-08-18 18:18:09 +0000

    created dashboard, login , home,register pages and added them to app.jsx using browserRoute

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,23 @@
+import {BrowserRouter , Routes , Route} from 'react-router-dom';
+
+import LoginPage from '../pages/LoginPage';
+import RegisterPage from '../pages/RegisterPage';
+import HomePage from '../pages/HomePage';
+import DashboardPage from '../pages/DashboardPage';
+
 function App(){
   return (
-    <div>
-      <h1>URL SHORTENER SERVICE</h1>
+    <BrowserRouter>
+    <div className="divcontainer">
+      <Routes>
+        <Route path='/' element={<HomePage/>}/>
+        <Route  path='/login' element ={<LoginPage/>}/>
+        <Route path ='/register' element ={<RegisterPage/>}/>
+        <Route path = '/dashboard' element ={<DashboardPage/>}/>
+      </Routes>
     </div>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
