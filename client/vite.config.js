0afbea5bd4ea07fd46e1d81d39859b5116fe0609
rishabhsharmaa <rishabh122005@gmail.com>
2025-08-18 17:39:14 +0000

    installed dependencies like axios and react-router-dom and updated vite configjs to handle api request for client with backend

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  //adding sever for configuration 
+  server : {
+    //proxy for api request
+    proxy : {
+      //any request which start with api should be provied to target
+      '/api' : {
+        //target express server
+        target : 'http://localhost:5000',
+        //it changes the origin header of the request to match with target url
+        changeOrigin : true,
+      },
+    }
+  }
 })
