fd155279eb12afa0a10d84d1c3d0af580f194a65
rishabhsharmaa <rishabh122005@gmail.com>
2025-08-21 18:42:38 +0000

    connected api with the react using axois in apiService which will shorten the user url

--- client/src/services/apiService.js
+++ client/src/services/apiService.js
@@ -1,4 +1,4 @@
-
+import axios from 'axios';
 /**
  * @desc    Sends a long URL to the backend API to be shortened.
  * @param   {string} longUrl The URL that the user wants to shorten.
@@ -6,3 +6,20 @@
  *          On success, this will be an object like: { success: true, data: { ...urlObject } }.
  *          On failure, the promise will be rejected with an error object.
  */
+
+export const createShortUrl = async (longUrl)=>{
+    try{
+        const response = await axios.post('/api/shorten',{longUrl});
+
+        return response.data;
+    }
+    catch(err){
+        console.error('API error , failed to create short Url!',err);
+        if(err.response && err.response.data){
+            throw err.response.data;
+        }
+        else{
+            throw new err('something went wrong,please try again!');
+        }
+    }
+};
