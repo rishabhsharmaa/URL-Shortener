9c217f719d3caa943bb985bcc1739a8da393dc05
Rishabh Sharma <144142029+rishabhsharmaa@users.noreply.github.com>
2026-05-24 17:06:13 +0530

    Update apiService.js

--- client/src/services/apiService.js
+++ client/src/services/apiService.js
@@ -1,4 +1,6 @@
 import axios from 'axios';
+import API_BASE_URL from './config';
+const API_URL = `${API_BASE_URL}/api/auth/`;
 /**
  * @desc    Sends a long URL to the backend API to be shortened.
  * @param   {string} longUrl The URL that the user wants to shorten.
