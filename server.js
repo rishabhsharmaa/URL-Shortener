4a9c5e41572aad1ed95205eb3cfeb1793d82742b
Rishabh Sharma <144142029+rishabhsharmaa@users.noreply.github.com>
2026-05-24 18:27:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -20,7 +20,10 @@ const app = express();
 connectDB();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ['https://linklytic.vercel.app', 'http://localhost:5173'],
+  credentials: true,
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
