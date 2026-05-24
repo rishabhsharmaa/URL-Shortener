1936e71511ecdd1e24ca7a4b7a8462b3c6bcc003
Rishabh Sharma <144142029+rishabhsharmaa@users.noreply.github.com>
2026-05-24 18:08:01 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,22 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from 'tailwindcss';
-// https://vite.dev/config/
+
 export default defineConfig({
-  plugins: [react(),
-  tailwindcss(),
-  ],
-  //adding sever for configuration 
-  server : {
-    //proxy for api request
-    proxy : {
-      //any request which start with api should be provied to target
-      '/api' : {
-        //target express server
-        target : 'http://localhost:5000',
-        //it changes the origin header of the request to match with target url
-        changeOrigin : true,
-      },
-    }
-  }
+  plugins: [react(), tailwindcss()],
 })
